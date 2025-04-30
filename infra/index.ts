f52e4e06faa444d1cc86ee3f48f04197eef77366
Chand <chandbud5@gmail.com>
2025-04-30 19:38:57 -0300

    feat: create chaotic app's deployment and svc

--- infra/index.ts
+++ infra/index.ts
@@ -1,14 +1,34 @@
 import * as k8s from "@pulumi/kubernetes";
+import * as yaml from "@pulumi/kubernetes/yaml";
 
-const appLabels = { app: "nginx" };
-const deployment = new k8s.apps.v1.Deployment("nginx", {
-    spec: {
-        selector: { matchLabels: appLabels },
-        replicas: 1,
-        template: {
-            metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
-        }
-    }
+const appLabels = { app: "chaotic-app" };
+
+const deployment = new k8s.apps.v1.Deployment("unstable-apis", {
+  spec: {
+    selector: { matchLabels: appLabels },
+    replicas: 3,
+    template: {
+      metadata: { labels: appLabels },
+      spec: {
+        containers: [{
+          name: "unstable-apis",
+          image: "chandbud5/chaotic-app:latest",
+          ports: [{ containerPort: 8000 }],
+        }],
+      },
+    },
+  },
 });
-export const name = deployment.metadata.name;
+
+const service = new k8s.core.v1.Service("unstable-apis-svc", {
+  metadata: { name: "unstable-apis" },
+  spec: {
+    type: "NodePort",
+    selector: appLabels,
+    ports: [{
+      port: 8000,
+      targetPort: 8000,
+      nodePort: 30080,
+    }],
+  },
+});
