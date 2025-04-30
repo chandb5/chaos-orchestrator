94c1463e46443e96e290b4cf49b65591e05d3bdc
Chand <chandbud5@gmail.com>
2025-04-30 19:24:58 -0300

    feat: add prometheus client to collect metrics

--- app/main.py
+++ app/main.py
@@ -1,44 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from prometheus_client import Counter, Summary, generate_latest, CONTENT_TYPE_LATEST
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
 
 import random
 import time
 
 app = FastAPI()
 
-REQUEST_COUNT = Counter("request_count", "Total number of requests")
+REQUEST_COUNT = Counter("request_count_total", "Total number of requests")
 REQUEST_LATENCY = Summary("request_latency_seconds", "Request latency in seconds")
+HTTP_STATUS_COUNTER = Counter(
+    "chaotic_http_responses_total",
+    "Count of HTTP responses by status code",
+    ["status_code"]
+)
+
+
+@app.middleware("http")
+async def status_code_metrics_middleware(request: Request, call_next):
+    response = await call_next(request)
+    HTTP_STATUS_COUNTER.labels(str(response.status_code)).inc()
+    return response
+
 
 @app.get("/")
 def index():
-    return {"message": "Chaos Ochestrator is running"}
+    REQUEST_COUNT.inc()
+    with REQUEST_LATENCY.time():
+        return JSONResponse({"message": "The message from chaotic app!"}, status_code=200)
 
 
 @app.get("/health")
 def health_check():
-    return {"status": "healthy"}
+    return JSONResponse({"status": "healthy"}, status_code=200)
 
 
 @app.get("/unstable")
 def unstable_endpoint():
-    x = random.randint(0, 12)
-    if x < 4:
-        raise Exception("Internal Server Error")
-    elif x < 7:
-        raise Exception("Service Unavailable")
-    elif x < 9:
-        raise Exception("Too Many Requests")
-    else:
-        return {"message": "Success"}
+    REQUEST_COUNT.inc()
+    with REQUEST_LATENCY.time():
+        x = random.randint(0, 12)
+        if x < 4:
+            return JSONResponse({"message": "Internal Server Error!"}, status_code=500)
+        elif x < 7:
+            return JSONResponse({"message": "Service Unavailable!"}, status_code=503)
+        elif x < 9:
+            return JSONResponse({"message": "Too Many Requests!"}, status_code=429)
+        else:
+            return JSONResponse({"message": "Request was successful"}, status_code=200)
 
 
 @app.get("/delay/{duration}")
 def delay_duration(duration: int):
-    time.sleep(duration)
-    return {"message": f"Delayed for {duration} seconds"}
+    REQUEST_COUNT.inc()
+    with REQUEST_LATENCY.time():
+        time.sleep(duration)
+        return JSONResponse({"message": f"Delayed for {duration} seconds"}, status_code=200)
 
 
 @app.get("/metrics")
 def metrics():
-    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+    return Response(
+        content=generate_latest(),
+        media_type=CONTENT_TYPE_LATEST,
+        status_code=200,
+    )
